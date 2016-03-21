16178ef89a36368cc032e7baaffc8bebfa11d98d
Fred <fcbsd@crowsons.com>
2016-03-21 23:44:40 +0000

    My Watch Face
    Watch Face - my version

--- src/main.c
+++ src/main.c
@@ -2,7 +2,9 @@
 
 static Window *s_main_window;
 
-static TextLayer *s_time_layer;
+static TextLayer *s_day_layer, *s_time_layer, *s_date_layer;
+
+static GFont s_time_font, s_date_font;
 
 static void update_time() {
   // Get tm structure
@@ -12,25 +14,55 @@ static void update_time() {
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
           "%H:%M" : "%I:%M", tick_time);
+  // Copy date into buffer from tm structure
+  static char date_buffer[16];
+  strftime(date_buffer, sizeof(date_buffer), "%d %b %y", tick_time);
+  // Copy day into buffer from tm structure
+  static char day_buffer[10];
+  strftime(day_buffer, sizeof(day_buffer), "%A", tick_time);
+  // Display the day
+  text_layer_set_text(s_day_layer, day_buffer);
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
+  // Display the date
+  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  // Create the TextLayer with specific bounds
+  // Set Fonts
+  s_time_font = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
+  s_date_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+  // Create Day Textlayer
+  s_day_layer = text_layer_create(
+    GRect(0, 10, 144, 30));
+  // Create Time TextLayer with specific bounds
   s_time_layer = text_layer_create(
     GRect(0, 52, bounds.size.w, 50));
+  // Create Date TextLayer
+  s_date_layer = text_layer_create(
+    GRect(0, 120, 144, 30));
+  // Day layout
+  text_layer_set_background_color(s_day_layer, GColorClear);
+  text_layer_set_text_color(s_day_layer, GColorBlack);
+  text_layer_set_font(s_day_layer, s_date_font);
+  text_layer_set_text_alignment(s_day_layer, GTextAlignmentCenter);
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
+  // Date layout
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_font(s_date_layer, s_date_font);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
   // Add it as a child layer to the window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_day_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -40,6 +72,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void main_window_unload(Window *window){
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
 }
 
 static void handle_init(void) {
