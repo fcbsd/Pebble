f1a44449a9f51023dfa61b40c9e096ce1f898b0a
Fred <fcbsd@crowsons.com>
2016-03-22 22:28:12 +0000

    Reset Watch Face to Black on Clear
    Re-order functions to see how it affects the Watch Face.

--- src/main.c
+++ src/main.c
@@ -8,28 +8,20 @@ static TextLayer *s_day_layer, *s_time_layer, *s_date_layer;
 static GFont s_time_font, s_date_font;
 
 static void update_time() {
+  static char s_buffer[8];
   /* Get tm structure */
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
   /* Write the current hours and minutes to buffer */
-  static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
           "%H:%M" : "%I:%M", tick_time);
-  /* Copy date into buffer from tm structure */
-  static char date_buffer[16];
-  strftime(date_buffer, sizeof(date_buffer), "%d %b %y", tick_time);
-  /* Copy day into buffer from tm structure */
-  static char day_buffer[10];
-  strftime(day_buffer, sizeof(day_buffer), "%A", tick_time);
-  /* Display the day */
-  text_layer_set_text(s_day_layer, day_buffer);
-  /* Display this time on the TextLayer */
+   /* Display this time on the TextLayer */
   text_layer_set_text(s_time_layer, s_buffer);
-  /* Display the date */
-  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void main_window_load(Window *window) {
+  static char day_buffer[10];
+  static char date_buffer[16];
   /* Get information about the Window */
   Layer *window_layer = window_get_root_layer(window);
   /* GRect(0,0,bounds.size.w,bounds.size.h) */
@@ -47,24 +39,35 @@ static void main_window_load(Window *window) {
   s_date_layer = text_layer_create(
     GRect(10, 120, 124, 30));
   /* Day layout */
-  text_layer_set_background_color(s_day_layer, GColorBlack);
-  text_layer_set_text_color(s_day_layer, GColorWhite);
+  text_layer_set_background_color(s_day_layer, GColorClear);
+  text_layer_set_text_color(s_day_layer, GColorBlack);
   text_layer_set_font(s_day_layer, s_date_font);
   text_layer_set_text_alignment(s_day_layer, GTextAlignmentCenter);
   /* Improve the layout to be more like a watchface */
-  text_layer_set_background_color(s_time_layer, GColorBlack);
-  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   /* Date layout */
-  text_layer_set_background_color(s_date_layer, GColorBlack);
-  text_layer_set_text_color(s_date_layer, GColorWhite);
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
   text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-  /* Add it as a child layers to the window's root layer */
+  /* Copy date into buffer from tm structure */
+  strftime(date_buffer, sizeof(date_buffer), "%d %b %y", tick_time);
+  /* Copy day into buffer from tm structure */
+  strftime(day_buffer, sizeof(day_buffer), "%A", tick_time);
+  /* Display the day */
+  text_layer_set_text(s_day_layer, day_buffer);
+  /* Display the date */
+  text_layer_set_text(s_date_layer, date_buffer);
+ /* Add it as a child layers to the window's root layer */
   layer_add_child(window_layer, text_layer_get_layer(s_day_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+  
+  /* Make sure the time is shown from the start */
+  update_time();
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -93,9 +96,6 @@ static void handle_init(void) {
   
   /* Show the window on the watch, with animated=true */
   window_stack_push(s_main_window, true);
-  
-  /* Make sure the time is shown from the start */
-  update_time();
 }
 
 void handle_deinit(void) {
