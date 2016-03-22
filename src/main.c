f0671b8ec9c1ddc6ed0496880ed709c1c4e996c0
Fred <fcbsd@crowsons.com>
2016-03-22 20:18:22 +0000

    Trying white text on black background

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+/* fwf: Fred's Watch Face */
 #include <pebble.h>
 
 static Window *s_main_window;
@@ -7,24 +8,24 @@ static TextLayer *s_day_layer, *s_time_layer, *s_date_layer;
 static GFont s_time_font, s_date_font;
 
 static void update_time() {
-  // Get tm structure
+  /* Get tm structure */
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
-  // Write the current hours and minutes to buffer
+  /* Write the current hours and minutes to buffer */
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
           "%H:%M" : "%I:%M", tick_time);
-  // Copy date into buffer from tm structure
+  /* Copy date into buffer from tm structure */
   static char date_buffer[16];
   strftime(date_buffer, sizeof(date_buffer), "%d %b %y", tick_time);
-  // Copy day into buffer from tm structure
+  /* Copy day into buffer from tm structure */
   static char day_buffer[10];
   strftime(day_buffer, sizeof(day_buffer), "%A", tick_time);
-  // Display the day
+  /* Display the day */
   text_layer_set_text(s_day_layer, day_buffer);
-  // Display this time on the TextLayer
+  /* Display this time on the TextLayer */
   text_layer_set_text(s_time_layer, s_buffer);
-  // Display the date
+  /* Display the date */
   text_layer_set_text(s_date_layer, date_buffer);
 }
 
@@ -35,31 +36,31 @@ static void main_window_load(Window *window) {
   // Set Fonts
   s_time_font = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
   s_date_font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
-  // Create Day Textlayer
+  /* Create Day Textlayer */
   s_day_layer = text_layer_create(
     GRect(0, 10, 144, 30));
-  // Create Time TextLayer with specific bounds
+  /* Create Time TextLayer with specific bounds */
   s_time_layer = text_layer_create(
     GRect(0, 52, bounds.size.w, 50));
-  // Create Date TextLayer
+  /* Create Date TextLayer */
   s_date_layer = text_layer_create(
     GRect(0, 120, 144, 30));
-  // Day layout
-  text_layer_set_background_color(s_day_layer, GColorClear);
-  text_layer_set_text_color(s_day_layer, GColorBlack);
+  /* Day layout */
+  text_layer_set_background_color(s_day_layer, GColorBlack);
+  text_layer_set_text_color(s_day_layer, GColorWhite);
   text_layer_set_font(s_day_layer, s_date_font);
   text_layer_set_text_alignment(s_day_layer, GTextAlignmentCenter);
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  /* Improve the layout to be more like a watchface */
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  // Date layout
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  /* Date layout */
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-  // Add it as a child layer to the window's root layer
+  /* Add it as a child layers to the window's root layer */
   layer_add_child(window_layer, text_layer_get_layer(s_day_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
@@ -70,33 +71,33 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void main_window_unload(Window *window){
-  // Destroy TextLayer
+  /* Destroy TextLayer */
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
 }
 
 static void handle_init(void) {
-  // Create main Window element and assign pointer
+  /* Create main Window element and assign pointer */
   s_main_window = window_create();
   
-  // Set handlers to manage the elemenet inside the window
+  /* Set handlers to manage the elemenet inside the window */
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
   
-  // Register with the TickTimerService
+  /* Register with the TickTimerService */
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
-  // Show the window on the watch, with animated=true
+  /* Show the window on the watch, with animated=true */
   window_stack_push(s_main_window, true);
   
-  // Make sure the time is shown from the start
+  /* Make sure the time is shown from the start */
   update_time();
 }
 
 void handle_deinit(void) {
-  // Destroy Window
+  /* Destroy Window */
   window_destroy(s_main_window);
 }
 
